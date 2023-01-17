566ed148a90c65aa8e8db64eb7d2a57857446597
Christian Castillo <castillochristian3@gmail.com>
2023-01-17 12:05:19 -0500

    created login and logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    #POST session login (create)
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: ["Incorrect username or password"]}, status: :unauthorized
+        end
+    end
+
+    #DELETE session logout (destroy)
+    def destroy
+        session.delete :user_id
+        render json: {}, status: :no_content
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get "/me", to: "users#show"
+
 end
